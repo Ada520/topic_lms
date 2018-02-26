98c9a6488b3c45fa1ff6a04976f0b2650c23dceb
DeepInEvil <deba.kgec@gmail.com>
2018-02-26 17:48:43 +0100

    saving awd_lstm_model

--- model.py
+++ model.py
@@ -19,6 +19,7 @@ class RNNModel(nn.Module):
         assert rnn_type in ['LSTM', 'QRNN', 'GRU'], 'RNN type is not supported'
         if rnn_type == 'LSTM':
             self.rnns = [torch.nn.LSTM(ninp if l == 0 else nhid, nhid if l != nlayers - 1 else (ninp if tie_weights else nhid), 1, dropout=0) for l in range(nlayers)]
+
             if wdrop:
                 self.rnns = [WeightDrop(rnn, ['weight_hh_l0'], dropout=wdrop) for rnn in self.rnns]
         if rnn_type == 'GRU':
@@ -79,6 +80,7 @@ class RNNModel(nn.Module):
         outputs = []
         for l, rnn in enumerate(self.rnns):
             current_input = raw_output
+            print rnn
             raw_output, new_h = rnn(raw_output, hidden[l])
             
             new_hidden.append(new_h)
