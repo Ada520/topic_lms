9310ad1105a9105a7dd7e6135f905d0136ad069a
DeepInEvil <deba.kgec@gmail.com>
2018-03-01 15:14:58 +0100

    saving awd_lstm_model

--- main.py
+++ main.py
@@ -78,9 +78,11 @@ if torch.cuda.is_available():
 
 corpus = data.Corpus(args.data)
 
+
 eval_batch_size = 10
 test_batch_size = 1
 train_data = batchify(corpus.train, args.batch_size, args)
+
 val_data = batchify(corpus.valid, eval_batch_size, args)
 test_data = batchify(corpus.test, test_batch_size, args)
 
