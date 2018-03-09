44bc1993368984babc25a5fa208899045ddeda03
DeepInEvil <deba.kgec@gmail.com>
2018-03-09 19:01:12 +0100

    change train val path

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from torch.autograd import Variable
 from embed_regularize import embedded_dropout
 from locked_dropout import LockedDropout
 from weight_drop import WeightDrop
-from LSTM_topic import LSTMCell
+from LSTM_topic import LSTMCell, LSTMtopicCell
 
 class RNNModel(nn.Module):
     """Container module with an encoder, a recurrent module, and a decoder."""
@@ -20,6 +20,136 @@ class RNNModel(nn.Module):
         assert rnn_type in ['LSTM', 'QRNN', 'GRU'], 'RNN type is not supported'
         if rnn_type == 'LSTM':
             self.rnns = [LSTMCell(ninp if l == 0 else nhid, nhid if l != nlayers - 1 else (ninp if tie_weights else nhid), bias=True) for l in range(nlayers)]
+            #if wdrop:
+            #    self.rnns = [WeightDrop(rnn, ['weight_hh_l0'], dropout=wdrop) for rnn in self.rnns]
+        if rnn_type == 'GRU':
+            self.rnns = [torch.nn.GRU(ninp if l == 0 else nhid, nhid if l != nlayers - 1 else ninp, 1, dropout=0) for l in range(nlayers)]
+            if wdrop:
+                self.rnns = [WeightDrop(rnn, ['weight_hh_l0'], dropout=wdrop) for rnn in self.rnns]
+        elif rnn_type == 'QRNN':
+            from torchqrnn import QRNNLayer
+            self.rnns = [QRNNLayer(input_size=ninp if l == 0 else nhid, hidden_size=nhid if l != nlayers - 1 else (ninp if tie_weights else nhid), save_prev_x=True, zoneout=0, window=2 if l == 0 else 1, output_gate=True) for l in range(nlayers)]
+            for rnn in self.rnns:
+                rnn.linear = WeightDrop(rnn.linear, ['weight'], dropout=wdrop)
+        #print(self.rnns)
+        self.rnns = torch.nn.ModuleList(self.rnns)
+        self.decoder = nn.Linear(nhid, ntoken)
+
+        # Optionally tie weights as in:
+        # "Using the Output Embedding to Improve Language Models" (Press & Wolf 2016)
+        # https://arxiv.org/abs/1608.05859
+        # and
+        # "Tying Word Vectors and Word Classifiers: A Loss Framework for Language Modeling" (Inan et al. 2016)
+        # https://arxiv.org/abs/1611.01462
+        if tie_weights:
+            #if nhid != ninp:
+            #    raise ValueError('When using the tied flag, nhid must be equal to emsize')
+            self.decoder.weight = self.encoder.weight
+
+        self.init_weights()
+
+        self.rnn_type = rnn_type
+        self.ninp = ninp
+        self.nhid = nhid
+        self.nlayers = nlayers
+        self.dropout = dropout
+        self.dropouti = dropouti
+        self.dropouth = dropouth
+        self.dropoute = dropoute
+        self.tie_weights = tie_weights
+
+    def reset(self):
+        if self.rnn_type == 'QRNN': [r.reset() for r in self.rnns]
+
+    def run_lstmcell(self, rnnmodel, input, hdn):
+        #print input.size()
+        #print hidden[0]
+        #print hdn
+        hx, cx = hdn
+        #print hx
+        #hx_all = Variable(input.size(0), input.size(1), )
+        hx_all = []
+        cx_all = []
+        #print hx.size(), cx.size()
+        #input = input.transpose(0, 1)
+        for j in range(input.size(0)):
+            #print input[j].size()
+            hx, cx = rnnmodel(input[j], (hx, cx))
+            hx_all.append(hx)
+            cx_all.append(cx)
+
+        #print hx
+        return torch.stack(hx_all), torch.stack(cx_all)
+
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.fill_(0)
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, input, hidden, return_h=False):
+        emb = embedded_dropout(self.encoder, input)
+
+        #emb = self.idrop(emb)
+
+        emb = self.lockdrop(emb, self.dropouti)
+
+        raw_output = emb
+        new_hidden = []
+        #raw_output, hidden = self.rnn(emb, hidden)
+        raw_outputs = []
+        outputs = []
+        for l, rnn in enumerate(self.rnns):
+            current_input = raw_output
+            #print rnn
+            raw_output, new_h = self.run_lstmcell(rnn, raw_output, hidden[l])
+            del(rnn)
+            new_hidden.append(new_h)
+            raw_outputs.append(raw_output)
+            if l != self.nlayers - 1:
+                #self.hdrop(raw_output)
+                raw_output = self.lockdrop(raw_output, self.dropouth)
+                outputs.append(raw_output)
+        #hidden = new_hidden
+        #hidden = raw_output[-1], new_hidden
+
+        output = self.lockdrop(raw_output, self.dropout)
+        outputs.append(output)
+
+        decoded = self.decoder(output.view(output.size(0)*output.size(1), output.size(2)))
+        result = decoded.view(output.size(0), output.size(1), decoded.size(1))
+        if return_h:
+            return result, raw_outputs, outputs
+        return result
+
+    def init_hidden(self, bsz):
+        weight = next(self.parameters()).data
+        if self.rnn_type == 'LSTM':
+            return [(Variable(torch.zeros(bsz, self.nhid if l != self.nlayers - 1 else (self.ninp if self.tie_weights else self.nhid)).cuda()),
+                    Variable(torch.zeros(bsz, self.nhid if l != self.nlayers - 1 else (self.ninp if self.tie_weights else self.nhid))).cuda())
+                    for l in range(self.nlayers)]
+            # return [(Variable(weight.new(1, bsz, self.nhid if l != self.nlayers - 1 else (self.ninp if self.tie_weights else self.nhid)).zero_()),
+            #         Variable(weight.new(1, bsz, self.nhid if l != self.nlayers - 1 else (self.ninp if self.tie_weights else self.nhid)).zero_()))
+            #         for l in range(self.nlayers)]
+        elif self.rnn_type == 'QRNN' or self.rnn_type == 'GRU':
+            return [Variable(weight.new(1, bsz, self.nhid if l != self.nlayers - 1 else (self.ninp if self.tie_weights else self.nhid)).zero_())
+                    for l in range(self.nlayers)]
+
+
+class RNNModel_mit_topic(nn.Module):
+    """Container module with an encoder, a recurrent module, and a decoder."""
+
+    def __init__(self, rnn_type, ntoken, ninp, nhid, nlayers, dropout=0.5, dropouth=0.5, dropouti=0.5, dropoute=0.1, wdrop=None, tie_weights=False, topic_size=False):
+        super(RNNModel_mit_topic, self).__init__()
+        self.lockdrop = LockedDropout()
+        self.idrop = nn.Dropout(dropouti)
+        self.hdrop = nn.Dropout(dropouth)
+        self.drop = nn.Dropout(dropout)
+        self.topic_size = topic_size
+        self.encoder = nn.Embedding(ntoken, ninp)
+        assert rnn_type in ['LSTM', 'QRNN', 'GRU'], 'RNN type is not supported'
+        if rnn_type == 'LSTM':
+            self.rnns = [LSTMtopicCell(ninp if l == 0 else nhid, nhid if l != nlayers - 1 else (ninp if tie_weights else nhid), bias=True, topic_size=self.topic_size) for l in range(nlayers)]
             #if wdrop:
             #    self.rnns = [WeightDrop(rnn, ['weight_hh_l0'], dropout=wdrop) for rnn in self.rnns]
         if rnn_type == 'GRU':
