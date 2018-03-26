34c633c78c962b8aac8e130b88eda316710cebc8
Debanjan Chaudhuri <DebanjanChaudhuri@qrowdgpu.aksw.uni-leipzig.de>
2018-03-26 12:57:48 +0200

    optimizing

--- preprocessing/preprocess_reviews.py
+++ preprocessing/preprocess_reviews.py
@@ -2,13 +2,13 @@ import glob
 import os
 import pickle
 import pandas as pd
-from nltk.tokenize import sent_tokenize
-from nltk.tokenize import word_tokenize
+#from nltk.tokenize import sent_tokenize
+#from nltk.tokenize import word_tokenize
 import gzip
-import ipdb
+#import ipdb
 import re
 import regex
-from nltk.corpus import stopwords
+#from nltk.corpus import stopwords
 import logging
 
 logger = logging.getLogger()
@@ -73,7 +73,7 @@ def preprocess_reviews(path, name):
         tokenized = pickle.load(f)
 
     logger.info("word tokenization")
-    word_tokenized = [[word_tokenize(sent) for sent in review] for review in tokenized]
+    word_tokenized = [[(sent+' eos').split() for sent in review] for review in tokenized]
 
     path = '/data/user/apopkes/data/amazon/word_tokenized_'+name
     with open(path, 'wb') as f:
@@ -81,7 +81,7 @@ def preprocess_reviews(path, name):
 
     logger.info("insert end-of-sentence markers")
     # add end-of-sentence markers to each sentence
-    [sent.append('eos') for review in word_tokenized for sent in review]
+    #[sent.append('eos') for review in word_tokenized for sent in review]
 
     path = '/data/user/apopkes/data/amazon/word_tokenized_eos_'+name
     with open(path, 'wb') as f:
@@ -93,7 +93,7 @@ if __name__ == "__main__":
 
     #file_list = glob.glob('/data/user/apopkes/data/amazon/raw/*')
     #file_list = glob.glob('/data/user/apopkes/data/amazon/raw/reviews_Home_and_Kitchen.json.gz')
-    file_list = glob.glob('/data/user/apopkes/data/amazon/temp_tokenized_Books')
+    file_list = glob.glob('/home/DebanjanChaudhuri/topic_lms/data/amazon/amazon_data/temp_tokenized_Books')
     #file_list = glob.glob('/data/user/apopkes/data/amazon/tokenized_Kitchen')
     # file_list = glob.glob('/data/user/apopkes/data/amazon/tokenized_Electronics')
 
