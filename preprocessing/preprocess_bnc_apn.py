2423c4fd9889500f499d0f9a648c250cff35ecbb
DeepInEvil <deba.kgec@gmail.com>
2018-04-13 18:09:39 +0200

    change train val path

--- preprocessing/preprocess_bnc_apn.py
+++ preprocessing/preprocess_bnc_apn.py
@@ -6,37 +6,49 @@ import os
 import glob
 
 ipdb.set_trace()
-filepaths = glob.glob(os.path.expanduser('~/topic_lms/data/apnews/apnews50k*'))
 
-for fpath in filepaths:
-    name = fpath.split(sep='/')[-1].split(sep='.')[-2]
-    with open(fpath, 'r') as f:
-        data = f.readlines()
-        processed = []
-        for report in data:
-            report = report.lower()
-            # Replace all tokens that contain numbers with the string "number"
-            report = re.sub("\S*\d\S*", "number", report)
-            # Remove multiple spaces between words
-            report = re.sub(' +',' ', report)
-            # Remove duplicated words in row
-            report = re.sub(r'\b(\w+)( \1\b)+', r'\1', report)
-            processed.append(report)
 
-        # tokenize each report into a list of sentences
-        processed = [report.split(sep='\t') for report in processed]
+def tokenize(corpora):
+    filepaths = glob.glob(os.path.expanduser('~/topic_lms/data/' + corpora + '/*'))
+    for fpath in filepaths:
+        if 'lda_models' in fpath:
+            continue
+        else:
+            name = fpath.split(sep='/')[-1].split(sep='.')[0]
+            print (name)
+            with open(fpath, 'r') as f:
+                data = f.readlines()
+                processed = []
+                for report in data:
+                    report = report.lower()
+                    # Replace all tokens that contain numbers with the string "number"
+                    report = re.sub("\S*\d\S*", "number", report)
+                    # Remove multiple spaces between words
+                    report = re.sub(' +',' ', report)
+                    # Remove duplicated words in row
+                    report = re.sub(r'\b(\w+)( \1\b)+', r'\1', report)
+                    processed.append(report)
 
-        # remove all punctuation
-        processed = [[re.sub('[^`A-Za-z0-9ÄÜÖßäüö ]+', '', sent) for sent in review] for review in processed]
+                # tokenize each report into a list of sentences
+                processed = [report.split(sep='\t') for report in processed]
 
-        # tokenize sentences into words
-        word_tokenized = [[word_tokenize(sent) for sent in review] for review in processed]
+                # remove all punctuation
+                processed = [[(re.sub('[^`A-Za-z0-9ÄÜÖßäüö ]+', '', sent) + ' eos' )for sent in review] for review in processed]
+                print (processed[0])
+                # tokenize sentences into words
+                word_tokenized = [[word_tokenize(sent) for sent in review] for review in processed]
 
-        # add end-of-sentence markers to each sentence
-        [sent.append('eos') for review in word_tokenized for sent in review]
-        ipdb.set_trace()
+                # add end-of-sentence markers to each sentence
+                #[sent.append('eos') for review in word_tokenized for sent in review]
+                ipdb.set_trace()
 
-        path = os.path.expanduser('~/topic_lms/data/preprocessed/word_tokenized_eos_' + name)
-        with open(path, 'wb') as f:
-            pickle.dump(word_tokenized, f)
+                path = os.path.expanduser('~/topic_lms/data/' + corpora + 'preprocessed/word_tokenized_eos_' + name)
+                with open(path, 'wb') as f:
+                    pickle.dump(word_tokenized, f)
+
+
+if __name__ == '__main__':
+    corpus = ['bnc', 'apnews', 'imdb']
+    for corp in corpus:
+        tokenize(corp)
 
