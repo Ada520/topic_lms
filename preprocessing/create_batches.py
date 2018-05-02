e4484909522d28eace29e3e24ebe18c6529d9ef7
zotroneneis <popkes@gmx.net>
2018-05-02 15:57:23 +0200

    create_batches.py

---
+++ preprocessing/create_batches.py
@@ -0,0 +1,24 @@
+import pickle
+import numpy as np
+import os
+import itertools
+
+
+corpus = 'bnc'
+train_path = os.path.expanduser('~/topic_lms/data/' + corpus + '/train_transform.pkl')
+
+with open(train_path, 'rb') as f:
+    data = pickle.load(f)
+
+
+# Create a padded batch
+# 1. Get 64 elements from list
+subset = data[:64]
+# 2. Create padding
+padded_subset = np.array(list(itertools.zip_longest(*subset, fillvalue=0))).T
+print('padded_subset: ', padded_subset)
+# 3. Create target batch
+subset_targets = np.roll(padded_subset, -1)
+subset_targets[:, -1] = 0
+print('subset_targets: ', subset_targets)
+
