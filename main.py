352f67a874e93fcaf359eac7517360346716c6b6
DeepInEvil <deba.kgec@gmail.com>
2018-05-03 21:52:36 +0200

    change train val path

--- main.py
+++ main.py
@@ -248,7 +248,7 @@ def evaluate(data_source, batch_size=10):
         output_flat = output.view(-1, ntokens)
         total_loss += len(data) * criterion(output_flat, targets).data
         #hidden = repackage_hidden(hidden)
-    return total_loss[0] / (data_source.shape[1])
+    return total_loss[0] / len(data_source)
 
 
 def train():
