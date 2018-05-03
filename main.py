29295931ec0c30729ae1785823350c44e2f57f53
DeepInEvil <deba.kgec@gmail.com>
2018-05-03 18:25:24 +0200

    change train val path

--- main.py
+++ main.py
@@ -314,7 +314,7 @@ def train():
         print (output.size())
         output = output.transpose(0, 1)
         print (output.size())
-        output = [o[:seqlen[i], :] for o, i in enumerate(output)]
+        output = [o[:seqlen[i], :] for i, o in enumerate(output)]
         print (seqlen[1])
         print (output[1].size())
         output = torch.cat(output, dim=1)
