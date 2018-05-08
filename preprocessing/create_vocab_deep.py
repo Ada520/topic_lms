7af92d14ba24c9560b7689023503cf6dca84c74e
DeepInEvil <deba.kgec@gmail.com>
2018-05-08 15:28:06 +0200

    change train val path

--- preprocessing/create_vocab_deep.py
+++ preprocessing/create_vocab_deep.py
@@ -75,16 +75,17 @@ def write_batches(raw_data, batch_size, num_steps, save_path, threshold=30):
     :return:
     """
     # Split long sentences into two parts
-    temp = [[sublist[:threshold], sublist[threshold:threshold+30], sublist[threshold+30:threshold+60], sublist[threshold+60:threshold+90], sublist[threshold+90:threshold+120], sublist[threshold+120:threshold+150], sublist[threshold+150:threshold+180], sublist[threshold+180:threshold:210], sublist[threshold+210:threshold+240], sublist[threshold+240:]]
-            if len(sublist) > threshold
-            else [sublist]
-            for sublist in raw_data]
-
-    temp_flat = [subsublist[:threshold]
-            for sublist in temp
-            for subsublist in sublist
-            if len(subsublist) > 0]
-
+    # temp = [[sublist[:threshold], sublist[threshold:threshold+30], sublist[threshold+30:threshold+60], sublist[threshold+60:threshold+90], sublist[threshold+90:threshold+120], sublist[threshold+120:threshold+150], sublist[threshold+150:threshold+180], sublist[threshold+180:threshold:210], sublist[threshold+210:threshold+240], sublist[threshold+240:]]
+    #         if len(sublist) > threshold
+    #         else [sublist]
+    #         for sublist in raw_data]
+    #
+    # temp_flat = [subsublist[:threshold]
+    #         for sublist in temp
+    #         for subsublist in sublist
+    #         if len(subsublist) > 0]
+    print (raw_data.shape)
+    temp_flat = [sent[:threshold] for sent in raw_data]
     # Save list to disk
     with open(save_path, 'wb') as f:
         pickle.dump(temp_flat, f)
